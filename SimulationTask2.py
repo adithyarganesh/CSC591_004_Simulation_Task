f87fdbf8988fced5bbac9ce89bdbc4359cec4853
Adithya Raghu Ganesh <adithyarganesh@gmail.com>
2020-09-09 15:38:34 -0400

    Add final submission

--- SimulationTask2.py
+++ SimulationTask2.py
@@ -12,14 +12,15 @@ def init():
     n_non_rt = 0
     server_status = 2
     scl = 4
-    #     print(f"{mc}\t{rt_clock}\t{non_rt_clock}\t{n_rt}\t{n_non_rt}\t{scl}\t{server_status}")
     return mc, rt_clock, non_rt_clock, n_rt, n_non_rt, scl, server_status
 
-
-def simulation(iat_rt, iat_non_rt, st_rt, st_non_rt, master_clock):
+# Static simulation with the initial arrival times and without the introduction of randomness
+def simulation(iat_rt, iat_non_rt, st_rt, st_non_rt, master_clock, Columns):
     mc, rt_clock, non_rt_clock, n_rt, n_non_rt, scl, server_status = init()
     non_rt_queue = []
     vals = [init()]
+
+    # Stopping condition
     while mc <= master_clock:
         if rt_clock < scl:
             mc = rt_clock
@@ -49,23 +50,24 @@ def simulation(iat_rt, iat_non_rt, st_rt, st_non_rt, master_clock):
                 server_status = 2
                 scl = mc + non_rt_queue[0]
                 non_rt_queue.pop(0)
-        #         print(f"{mc}\t{rt_clock}\t{non_rt_clock}\t{n_rt}\t{n_non_rt}\t{scl}\t{server_status}")
         vals.append((mc, rt_clock, non_rt_clock, n_rt, n_non_rt, scl, server_status))
+        
         # To check if the server would get idle
         if scl > rt_clock and scl > non_rt_clock:
             server_status = 0
 
         mc = scl
 
-    return pd.DataFrame(vals, columns=["Master Clock", "RT Clock", "NON-RT Clock", "N_RT", "N_NON-RT", "SCL",
-                                       "Server Status"])
+    return pd.DataFrame(vals, columns=Columns)
 
 
+# Add randomness based on arrival and processing time
 def sample(time):
     return -1 * time * math.log(random.uniform(0, 1))
 
 
-def simulation_with_randomness(iat_rt, iat_non_rt, st_rt, st_non_rt, master_clock):
+# Dynamic simulation with the initial arrival times and with the introduction of randomness
+def simulation_with_randomness(iat_rt, iat_non_rt, st_rt, st_non_rt, master_clock, Columns):
     mc, rt_clock, non_rt_clock, n_rt, n_non_rt, scl, server_status = init()
     non_rt_queue = []
     vals = [init()]
@@ -100,7 +102,6 @@ def simulation_with_randomness(iat_rt, iat_non_rt, st_rt, st_non_rt, master_cloc
                 scl = round(mc + non_rt_queue[0], 2)
                 non_rt_queue.pop(0)
         vals.append((mc, rt_clock, non_rt_clock, n_rt, n_non_rt, scl, server_status))
-        #         print(f"{mc}\t{rt_clock}\t{non_rt_clock}\t{n_rt}\t{n_non_rt}\t{scl}\t{server_status}")
 
         if n_non_rt == 0 and n_rt == 0:
             server_status = 0
@@ -108,25 +109,20 @@ def simulation_with_randomness(iat_rt, iat_non_rt, st_rt, st_non_rt, master_cloc
 
         mc = scl
 
-    return pd.DataFrame(vals, columns=["Master Clock", "RT Clock", "NON-RT Clock", "N_RT", "N_NON-RT", "SCL",
-                                       "Server Status"])
+    return pd.DataFrame(vals, columns=Columns)
 
 
 # Main function
 if __name__ == '__main__':
-    empty = pd.DataFrame([("", "", "", "", "", "", "")],
-                         columns=["Master Clock", "RT Clock", "NON-RT Clock", "N_RT", "N_NON-RT", "SCL",
-                                  "Server Status"])
-    #     print("Task 2.1\nSubtask 1.1\nmc\trt_clock\tnon_rt_clock\tn_rt\tn_non_rt\tscl\tserver_status")
-    df1 = simulation(iat_rt=10, iat_non_rt=5, st_rt=2, st_non_rt=4, master_clock=50)
-    #     print("\nTask 2.1\nSubtask 1.2\nmc\trt_clock\tnon_rt_clock\tn_rt\tn_non_rt\tscl\tserver_status")
-    df2 = simulation(iat_rt=5, iat_non_rt=10, st_rt=4, st_non_rt=2, master_clock=20)
+    Columns = ["Master Clock", "RT Clock", "NON-RT Clock", "N_RT", "N_NON-RT", "SCL", "Server Status"]
+    empty = pd.DataFrame([("", "", "", "", "", "", "")], columns=Columns)
+    
+    df1 = simulation(iat_rt=10, iat_non_rt=5, st_rt=2, st_non_rt=4, master_clock=50, Columns=Columns)
+    df2 = simulation(iat_rt=5, iat_non_rt=10, st_rt=4, st_non_rt=2, master_clock=20, Columns=Columns)
     result = pd.concat([df1, empty, empty, df2])
-    #     print("Task 2.2\nSubtask 1.1\nmc\trt_clock\tnon_rt_clock\tn_rt\tn_non_rt\tscl\tserver_status")
-    df3 = simulation_with_randomness(iat_rt=10, iat_non_rt=5, st_rt=2, st_non_rt=4, master_clock=200)
-    #     print("\nTask 2.2\nSubtask 1.2\nmc\trt_clock\tnon_rt_clock\tn_rt\tn_non_rt\tscl\tserver_status")
-    df4 = simulation_with_randomness(iat_rt=5, iat_non_rt=10, st_rt=4, st_non_rt=2, master_clock=200)
-    result2 = pd.concat([df3, empty, empty, df4])
-
     result.to_csv("Result_Task2-1.csv", index=False)
-    result2.to_csv("Result_Task2-2.csv", index=False)
+
+    df1 = simulation_with_randomness(iat_rt=10, iat_non_rt=5, st_rt=2, st_non_rt=4, master_clock=200, Columns=Columns)
+    df2 = simulation_with_randomness(iat_rt=5, iat_non_rt=10, st_rt=4, st_non_rt=2, master_clock=200, Columns=Columns)
+    result = pd.concat([df1, empty, empty, df2])
+    result.to_csv("Result_Task2-2.csv", index=False)
